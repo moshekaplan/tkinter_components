5ae40fdb1945f3900ab3f353bb7cd769875cdffd
Moshe Kaplan <me@moshekaplan.com>
2015-03-23 18:08:51 -0400

    Modified an example to include a Tkinter entry field

--- CalendarDialog/CalendarDialog.py
+++ CalendarDialog/CalendarDialog.py
@@ -3,6 +3,7 @@ import ttkcalendar
 
 import tkSimpleDialog
 
+
 class CalendarDialog(tkSimpleDialog.Dialog):
     """Dialog box that displays a calendar and returns the selected date"""
     def body(self, master):
@@ -11,22 +12,30 @@ class CalendarDialog(tkSimpleDialog.Dialog):
 
     def apply(self):
         self.result = self.calendar.selection
-        
+
 # Demo code:
+
+
+class CalendarFrame(Tkinter.LabelFrame):
+    def __init__(self, master):
+        Tkinter.LabelFrame.__init__(self, master, text="CalendarDialog Demo")
+
+        def getdate():
+            cd = CalendarDialog(self)
+            result = cd.result
+            self.selected_date.set(result.strftime("%m/%d/%Y"))
+
+        self.selected_date = Tkinter.StringVar()
+
+        Tkinter.Entry(self, textvariable=self.selected_date).pack(side=Tkinter.LEFT)
+        Tkinter.Button(self, text="Choose a date", command=getdate).pack(side=Tkinter.LEFT)
+
+
 def main():
     root = Tkinter.Tk()
     root.wm_title("CalendarDialog Demo")
-    
-    def onclick():
-        cd = CalendarDialog(root)
-        print cd.result
-
-    button = Tkinter.Button(root, text="Click me to see a calendar!", command=onclick)
-    button.pack()
-    root.update()
-
+    CalendarFrame(root).pack()
     root.mainloop()
-    
 
 if __name__ == "__main__":
     main()
